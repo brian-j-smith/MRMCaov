c3f566ac5b2333d83af0e4ff95e574ef7d0ba679
Brian J Smith <brian-j-smith@uiowa.edu>
2019-01-18 13:27:55 -0600

    Implement jackknife covariance method

---
+++ R/jackknife.R
@@ -0,0 +1,27 @@
+#' @rdname cov_methods
+#' 
+jackknife <- function() {
+  structure(
+    function(formula, data) {
+      vars <- extract_vars(formula)
+      cases <- data[["(cases)"]]
+      groups <- interaction(data[[vars["tests"]]], data[[vars["readers"]]])
+      df <- data[vars[c("observed", "predicted")]]
+      
+      metrics <- matrix(NA, nlevels(cases), nlevels(groups))
+      for (i in 1:nlevels(cases)) {
+        include <- cases != levels(cases)[i]
+        metrics[i, ] <- by(df[include, ], groups[include], function(split) {
+          eval(formula[[2]], split)
+        })
+      }
+      
+      n <- nrow(metrics)
+      structure(
+        ((n - 1) / n) * crossprod(scale(metrics, scale = FALSE)),
+        dimnames = list(levels(groups), levels(groups))
+      )
+    },
+    class = c("cov_method", "function")
+  )
+}
