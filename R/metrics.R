52c973ae095616fa1c321cc1619121e05085ac8c
Brian J Smith <brian-j-smith@uiowa.edu>
2019-06-05 15:07:10 -0500

    Change metrics argument names to truth and rating

--- R/metrics.R
+++ R/metrics.R
@@ -8,15 +8,15 @@ psi <- function(x_pos, x_neg) {
 #' @name metrics
 #' @rdname metrics
 #' 
-#' @param observed vector of observed binary responses.
-#' @param predicted vector of predcited responses.
+#' @param truth vector of true binary responses.
+#' @param rating vector of ratings.
 #' 
-binormroc_auc <- function(observed, predicted) {
-  observed <- as.factor(observed)
-  is_pos <- observed == levels(observed)[2]
+binormroc_auc <- function(truth, rating) {
+  truth <- as.factor(truth)
+  is_pos <- truth == levels(truth)[2]
 
-  pred_pos <- as.double(predicted[is_pos])
-  pred_neg <- as.double(predicted[!is_pos])
+  pred_pos <- as.double(rating[is_pos])
+  pred_neg <- as.double(rating[!is_pos])
   
   fit <- .Fortran("cvbmroc",
                   length(pred_neg), length(pred_pos), pred_neg, pred_pos,
@@ -27,12 +27,12 @@ binormroc_auc <- function(observed, predicted) {
 
 #' @rdname metrics
 #' 
-proproc_auc <- function(observed, predicted) {
-  observed <- as.factor(observed)
-  is_pos <- observed == levels(observed)[2]
+proproc_auc <- function(truth, rating) {
+  truth <- as.factor(truth)
+  is_pos <- truth == levels(truth)[2]
 
-  pred_pos <- as.double(predicted[is_pos])
-  pred_neg <- as.double(predicted[!is_pos])
+  pred_pos <- as.double(rating[is_pos])
+  pred_neg <- as.double(rating[!is_pos])
   
   fit <- .Fortran("pbmroc",
                   length(pred_neg), length(pred_pos), pred_neg, pred_pos,
@@ -43,24 +43,24 @@ proproc_auc <- function(observed, predicted) {
 
 #' @rdname metrics
 #' 
-roc_auc <- function(observed, predicted) {
-  observed <- as.factor(observed)
+roc_auc <- function(truth, rating) {
+  truth <- as.factor(truth)
   
-  R <- rank(predicted)
-  is_pos <- observed == levels(observed)[2]
+  R <- rank(rating)
+  is_pos <- truth == levels(truth)[2]
   num_pos <- sum(is_pos)
-  num_neg <- length(observed) - num_pos
+  num_neg <- length(truth) - num_pos
   (sum(R[is_pos]) - num_pos * (num_pos + 1) / 2) / (num_pos * num_neg)
 }
 
 
 #' ROC Performance Metrics
 #' 
-#' Calculation of TPR and FPR pairs for all values of a numeric predictor
-#' of an observed binary response.
+#' Calculation of TPR and FPR pairs for all values of a numeric rating of a
+#' true binary response.
 #' 
-#' @param observed vector of observed binary responses.
-#' @param predicted vector of predcited responses.
+#' @param truth vector of true binary responses.
+#' @param rating vector of ratings.
 #' @param ... interaction factors within which to calculate metrics.
 #' 
 #' @seealso \code{\link{plot}}
@@ -68,17 +68,17 @@ roc_auc <- function(observed, predicted) {
 #' @examples
 #' with(VanDyke, roc(truth, rating, treatment, reader))
 #' 
-roc <- function(observed, predicted, ...) {
+roc <- function(truth, rating, ...) {
   groups <- list(...)
-  if (length(groups) == 0) groups <- list(rep(1, length(observed)))
+  if (length(groups) == 0) groups <- list(rep(1, length(truth)))
   if (is.null(names(groups))) {
     names(groups) <- make.unique(rep("Group", length(groups)))
   }
   groups <- lapply(groups, as.character)
   
-  df <- data.frame(observed, predicted, groups)
+  df <- data.frame(truth, rating, groups)
   perf_list <- by(df, groups, function(data) {
-    metrics <- .roc(data$observed, data$predicted)
+    metrics <- .roc(data$truth, data$rating)
     data.frame(data[rep(1, nrow(metrics)), -(1:2), drop = FALSE],
                metrics, row.names = NULL)
   }, simplify = FALSE)
@@ -91,8 +91,8 @@ roc <- function(observed, predicted, ...) {
 }
 
 
-.roc <- function(observed, predicted) {
-  perf <- ROCR::prediction(predicted, observed) %>%
+.roc <- function(truth, rating) {
+  perf <- ROCR::prediction(rating, truth) %>%
     ROCR::performance("tpr", "fpr")
   cbind(FPR = perf@x.values[[1]], TPR = perf@y.values[[1]])
 }
