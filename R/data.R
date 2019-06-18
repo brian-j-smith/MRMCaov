51de3301a6ec40e8dc32d2790073dff8dc6d9d0a
Brian J Smith <brian-j-smith@uiowa.edu>
2019-06-18 11:13:11 -0500

    Add nested case variables to VanDyke dataset

--- R/data.R
+++ R/data.R
@@ -4,7 +4,9 @@
 #' \describe{
 #'   \item{reader}{reader identifier}
 #'   \item{treatment}{treatment identifier}
-#'   \item{case}{case identifier}
+#'   \item{case}{case identifier (factorial design)}
+#'   \item{case2}{case identifier (cases nested within readers)}
+#'   \item{case3}{case identifier (cases nested within treatments)}
 #'   \item{truth}{true case status (1 = positive, 0 = negative)}
 #'   \item{rating}{ordinal reader ratings of case status}
 #' }
