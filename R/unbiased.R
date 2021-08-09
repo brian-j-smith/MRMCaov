a296c6496f963e115e382b7bc91f470e033e79df
Brian J Smith <brian-j-smith@uiowa.edu>
2021-08-09 13:32:14 -0500

    Code edits: simplify unbiased() loops

--- R/unbiased.R
+++ R/unbiased.R
@@ -73,30 +73,28 @@ unbiased <- function() {
 
 
   n <- nlevels(data$group)
-  a1 <- a2 <- a3 <- a4 <- matrix(NA, n, n)
-  indices <- expand.grid(i = 1:n, j = 1:n)
-  indices <- indices[indices$j <= indices$i, ]
+  A1 <- A2 <- A3 <- A4 <- matrix(NA, n, n)
 
   pb <- progress_bar$new(
     format = "Computing unbiased covariance: [:bar] :percent | :eta",
-    total = nrow(indices)
+    total = n * (n + 1) / 2
   )
-  for (k in 1:nrow(indices)) {
-    pb$tick()
-    i <- indices$i[k]
-    j <- indices$j[k]
-    psi_cross <- tcrossprod(psi_list[[i]], psi_list[[j]])
-    a1[i, j] <- a1[j, i] <- mean(psi_cross[in_a1])
-    a2[i, j] <- a2[j, i] <- mean(psi_cross[in_a2])
-    a3[i, j] <- a3[j, i] <- mean(psi_cross[in_a3])
-    a4[i, j] <- a4[j, i] <- mean(psi_cross[in_a4])
+  for (i in 1:n) {
+    for (j in 1:i) {
+      pb$tick()
+      psi_cross <- tcrossprod(psi_list[[i]], psi_list[[j]])
+      A1[i, j] <- A1[j, i] <- mean(psi_cross[in_a1])
+      A2[i, j] <- A2[j, i] <- mean(psi_cross[in_a2])
+      A3[i, j] <- A3[j, i] <- mean(psi_cross[in_a3])
+      A4[i, j] <- A4[j, i] <- mean(psi_cross[in_a4])
+    }
   }
   pb$terminate()
 
-  covmat <- (a1 + (n_neg - 1) * a2 + (n_pos - 1) * a3 +
-               (1 - n_pos - n_neg) * a4) / (n_pos * n_neg)
+  covmat <- (A1 + (n_neg - 1) * A2 + (n_pos - 1) * A3 +
+               (1 - n_pos - n_neg) * A4) / (n_pos * n_neg)
 
-  structure(covmat, a = list(a1, a2, a3, a4))
+  structure(covmat, a = list(A1, A2, A3, A4))
 
 }
 
@@ -114,41 +112,33 @@ unbiased <- function() {
   })
 
   n <- nlevels(data$group)
-  a1 <- a2 <- a3 <- a4 <- matrix(NA, n, n)
-  indices <- expand.grid(i = 1:n, j = 1:n)
-  indices <- indices[indices$j <= indices$i, ]
+  A1 <- A2 <- A3 <- A4 <- matrix(NA, n, n)
 
   pb <- progress_bar$new(
     format = "Computing unbiased covariance: [:bar] :percent | :eta",
-    total = nrow(indices)
+    total = n * (n + 1) / 2
   )
-  for (k in 1:nrow(indices)) {
-    pb$tick()
+  for (i in 1:n) {
+    for (j in 1:i) {
+      psi_cross <- tcrossprod(df_list[[i]]$psi, df_list[[j]]$psi)
+      same_case_pos <- outer(df_list[[i]]$case_pos, df_list[[j]]$case_pos, "==")
+      same_case_neg <- outer(df_list[[i]]$case_neg, df_list[[j]]$case_neg, "==")
 
-    i <- indices$i[k]
-    j <- indices$j[k]
-
-    df_i <- df_list[[i]]
-    df_j <- df_list[[j]]
-
-    psi_cross <- tcrossprod(df_i$psi, df_j$psi)
-    same_case_pos <- outer(df_i$case_pos, df_j$case_pos, "==")
-    same_case_neg <- outer(df_i$case_neg, df_j$case_neg, "==")
-
-    mean_of <- function(keep) mean(psi_cross[keep])
-    a1[i, j] <- a1[j, i] <- mean_of(same_case_pos & same_case_neg)
-    a2[i, j] <- a2[j, i] <- mean_of(same_case_pos & !same_case_neg)
-    a3[i, j] <- a3[j, i] <- mean_of(!same_case_pos & same_case_neg)
-    a4[i, j] <- a4[j, i] <- mean_of(!same_case_pos & !same_case_neg)
+      mean_of <- function(keep) mean(psi_cross[keep])
+      A1[i, j] <- A1[j, i] <- mean_of(same_case_pos & same_case_neg)
+      A2[i, j] <- A2[j, i] <- mean_of(same_case_pos & !same_case_neg)
+      A3[i, j] <- A3[j, i] <- mean_of(!same_case_pos & same_case_neg)
+      A4[i, j] <- A4[j, i] <- mean_of(!same_case_pos & !same_case_neg)
+    }
   }
   pb$terminate()
 
   n_pos <- sum(!duplicated(df_pos$case))
   n_neg <- sum(!duplicated(df_neg$case))
-  covmat <- (a1 + (n_neg - 1) * a2 + (n_pos - 1) * a3 +
-               (1 - n_pos - n_neg) * a4) / (n_pos * n_neg)
+  covmat <- (A1 + (n_neg - 1) * A2 + (n_pos - 1) * A3 +
+               (1 - n_pos - n_neg) * A4) / (n_pos * n_neg)
 
-  structure(covmat, a = list(a1, a2, a3, a4))
+  structure(covmat, a = list(A1, A2, A3, A4))
 
 }
 
